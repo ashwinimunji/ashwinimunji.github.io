8086621930c1efcc5bdac21964632a64d7b6c659
Arjun Munji <arjunmunji@hotmail.com>
2015-04-30 01:44:22 +0530

    wp-feed
    trying automation

---
+++ js/wp-feed.js
@@ -0,0 +1,15 @@
+window.current_url = "http://ashwinimunji.com/blog/the-laptop/?json=1";
+$.getJSON(current_url, function(data) {
+	window.globalData = data;
+	console.log(globalData);
+	document.getElementById("wp-feed").innerHTML=document.getElementById("wp-feed").innerHTML + '<h4><a href="'+data.post.url+'">'+data.post.title+'</a></h4>';
+	document.getElementById("wp-feed").innerHTML=document.getElementById("wp-feed").innerHTML + '<p><a href="'+data.post.url+'"<span class="image right"><img src="'+data.post.thumbnail_images.medium.url+'"</span></a>'+data.post.excerpt+'<a href="'+data.post.url+'"> Read More &#8594;</p>';
+	console.log(current_url);
+});
+console.log(current_url);
+console.log(globalData);
+while(globalData.next_url)
+{
+	current_url=globalData.next_url+'?json=1';
+	$.getJSON();
+}
